296f3bf6ef05efd5be3f085439d7d4ca3de690da
Neema Velasco <neemavelasco@gmail.com>
2022-04-04 15:23:48 -0500

    fixed schema errors and added initial seeds

--- db/schema.sql
+++ db/schema.sql
@@ -5,13 +5,14 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30),
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id) 
         REFERENCES department(id)
         ON DELETE SET NULL
@@ -21,6 +22,7 @@ CREATE TABLE employee (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_id INT,
     FOREIGN KEY (role_id)
         REFERENCES role(id)
         ON DELETE SET NULL,
