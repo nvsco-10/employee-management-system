9b7d76750bee1f49f6f6fbf4e2d26bb54acc9dd0
Neema Velasco <neemavelasco@gmail.com>
2022-04-04 15:15:04 -0500

    schema refactor and add manager_id

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,28 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30),
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (department_id) 
-    REFERENCES department(id)
-    ON DELETE SET NULL
+        REFERENCES department(id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    manager_id
-    PRIMARY KEY (id),
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL
-)
+        REFERENCES role(id)
+        ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+        REFERENCES employee(id)
+        ON DELETE SET NULL
+);
