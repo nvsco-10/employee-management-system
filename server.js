d7602572c5257f11e030182a9d0e665f1b58d6c9
Neema Velasco <neemavelasco@gmail.com>
2022-04-04 16:09:05 -0500

    added prompt questions and switch case for questions

--- server.js
+++ server.js
@@ -9,6 +9,57 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const questions = [
+    {
+        type: "list",
+        name: "task",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+            "Exit"
+        ]
+    }
+];
+
+async function loadDb() {
+    const result = await inquirer.prompt(questions);
+
+    switch(result.task) {
+        case "View all departments":
+            viewDepartments();
+            break;
+        case "View all roles":
+            viewRoles();
+            break;
+        case "View all employees":
+            viewEmployees();
+            break;      
+        case "Add a department":
+            addDepertment();
+            break;     
+        case "Add a role":
+            addRole();
+            break;  
+        case "Add an employee":
+            addEmployee();
+            break;  
+        case "Update an employee role":
+            updateRole();
+            break;
+        case "Exit":
+            return '';
+            break;    
+    }
+}
+
+loadDb();
+
 const db = mysql.createConnection(
     {
         host: 'localhost',
