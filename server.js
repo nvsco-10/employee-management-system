b083ffba86785e9d6449eb7b07f52dc6afea1375
Neema Velasco <neemavelasco@gmail.com>
2022-04-04 15:50:22 -0500

    established connection to db in server.js

--- server.js
+++ server.js
@@ -9,6 +9,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'pineapple',
+        database: 'employee_db'
+    },
+    console.log('Connected to the employee_db database.')
+);
+
+// db.query('SELECT * FROM employee', (err, results) => {
+//     console.log(results);
+// })
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
